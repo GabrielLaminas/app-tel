2206734a9339e1dacae8cdec3e8a142e29c2d356
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2024-05-12 12:42:21 -0300

    Configuração de Rotas

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { View, Text } from "react-native";
-import React from "react";
-import Login from "./src/Pages/Login/Login.js";
-import Conta from "./src/Pages/Conta/Conta.js";
-import Contato from "./src/Pages/Contato/Contato.js";
-import ContatoUsuario from "./src/Pages/ContatoUsuario/ContatoUsuario.js";
+import {View, Text} from 'react-native';
+import React from 'react';
+import Login from './src/Pages/Login/Login.js';
+import Conta from './src/Pages/Conta/Conta.js';
+import Contato from './src/Pages/Contato/Contato.js';
+import ContatoUsuario from './src/Pages/ContatoUsuario/ContatoUsuario.js';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <Login />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
