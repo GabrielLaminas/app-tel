e5df5103360d7dc5809592cd8e6566b087048dd7
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2024-06-27 19:49:10 -0300

    Criação de layout para tela Contato Usuário

--- src/Pages/ContatoUsuario/ContatoUsuarioStyle.js
+++ src/Pages/ContatoUsuario/ContatoUsuarioStyle.js
@@ -1,51 +1,91 @@
 import styled from "styled-components/native";
 
 const MainView = styled.View`
-   padding: 16px 16px 0 16px;
+   padding: 0 16px;
    flex: 1;
+   background-color: #fff;
 `;
 
-const MainTitle = styled.Text`
+const ViewName = styled.View`
+   margin-top: 40px;
+   gap: 16px;
+   align-items: center;
+`;
+
+const ViewNameCircle = styled.View`
+   width: 150px;
+   height: 150px;
+   justify-content: center;
+   align-items: center;
+   background-color: #F3F5F7;
+   border-radius: 85px;
+`;
+
+const ViewNameCircleLetter = styled.Text`
+   color: #1A1E23;
    font-size: 32px;
+   font-weight: 600;
    text-align: center;
-   font-weight: 700;
+`;
+
+const ViewFullName = styled.Text`
+   color: #185A3D;
+   font-size: 20px;
+   font-weight: 500;
+   text-align: center;
 `;
 
 const ViewIcons = styled.View`
-   margin: 30px 0 32px 0;
+   margin: 24px 0 42px 0;
    display: flex;
    flex-direction: row;
-   justify-content: flex-end;
-   gap: 20px;
+   justify-content: center;
+   gap: 42px;
+`;
+
+const ViewIconsGroup = styled.View`
+   gap: 8px;
+   align-items: center;
 `;
 
 const ViewIconsButton = styled.TouchableOpacity`
-   width: 50px;
-   height: 50px;
+   width: 44px;
+   height: 44px;
    align-items: center;
    justify-content: center;
-   background-color: gray;
+   background-color: #F3F5F7;
    border-radius: 50px;
 `;
 
+const ViewIconsGroupText = styled.Text`
+   color: #1A1E23;
+   font-size: 14px;
+   font-weight: 400;
+`;
+
 const ViewInfos = styled.View`
-   gap: 36px;
+   gap: 20px;
 `;
 
 const ViewInfosItem = styled.View`
-   gap: 12px;
+   padding-bottom: 20px;
+   gap: 10px;
+   border-bottom-color: ${props => props.borderB ? 'rgba(217, 217, 217, 0.4)' : 'auto'};
+   border-bottom-width: 1px;
 `;
 
 const ViewInfosItemTitle = styled.Text`
+   color: #185A3D;
    font-size: 24px;
    font-weight: 600;
 `;
 
 const ViewInfosItemBody = styled.Text`
-   font-size: 24px;
+   color: #45505E;
+   font-size: 20px;
    font-weight: 400;
 `;
 
 export {
-   MainView, MainTitle, ViewIcons, ViewIconsButton, ViewInfos, ViewInfosItem, ViewInfosItemTitle, ViewInfosItemBody
+   MainView, ViewName, ViewNameCircle, ViewNameCircleLetter, ViewFullName,  ViewIcons, ViewIconsButton, ViewIconsGroup, ViewIconsGroupText,  ViewInfos, ViewInfosItem, ViewInfosItemTitle, ViewInfosItemBody
 }
